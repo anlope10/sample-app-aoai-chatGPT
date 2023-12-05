697471275be58b0004ce7765e804d95ea6dcb8a2
anlope10 <anlope10@gmail.com>
2023-12-05 15:19:59 +0100

    Icon change
    Changes in icon and text to customize

--- frontend/src/pages/chat/Chat.tsx
+++ frontend/src/pages/chat/Chat.tsx
@@ -9,7 +9,7 @@ import uuid from 'react-uuid';
 import { isEmpty } from "lodash-es";
 
 import styles from "./Chat.module.css";
-import Azure from "../../assets/Azure.svg";
+import Azure from "../../assets/GC_icon.svg";
 
 import {
     ChatMessage,
@@ -585,8 +585,8 @@ const Chat = () => {
                                     className={styles.chatIcon}
                                     aria-hidden="true"
                                 />
-                                <h1 className={styles.chatEmptyStateTitle}>Start chatting</h1>
-                                <h2 className={styles.chatEmptyStateSubtitle}>This chatbot is configured to answer your questions</h2>
+                                <h1 className={styles.chatEmptyStateTitle}>Puedes comenzar a chatear</h1>
+                                <h2 className={styles.chatEmptyStateSubtitle}>Este chatbot está diseñado para contestar preguntas sobre sistemas informaticos</h2>
                             </Stack>
                         ) : (
                             <div className={styles.chatMessageStream} style={{ marginBottom: isLoading ? "40px" : "0px"}} role="log">
